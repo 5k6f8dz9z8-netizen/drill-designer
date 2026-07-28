c3017d6614e39c08d10b8407e115bf0732291e98
5k6f8dz9z8-netizen <5k6f8dz9z8@privaterelay.appleid.com>
2026-07-28 11:29:51 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Drill Designer service worker.
    Bump CACHE whenever you change index.html so phones pick up the new version. */
-const CACHE = "drill-designer-v1";
+const CACHE = "drill-designer-v2";
 const CORE = [
   "./",
   "./index.html",
