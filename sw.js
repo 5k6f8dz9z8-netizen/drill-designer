b29c1952a8726e06d97b6dfea442f89f4db8e1ec
5k6f8dz9z8-netizen <5k6f8dz9z8@privaterelay.appleid.com>
2026-07-30 09:06:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Rondo service worker.
    Bump CACHE whenever you change index.html so phones pick up the new version. */
-const CACHE = "rondo-v5";
+const CACHE = "rondo-v9";
 const CORE = [
   "./",
   "./index.html",
